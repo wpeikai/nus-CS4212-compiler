714eb80d345d39eefe92a067cdd0a43d189d5556
acreux <antoine.creux@gmail.com>
2014-10-19 14:54:47 +0800

    Rename test files. Rename readme and script to run test files

--- run_tests.sh
+++ run_tests.sh
@@ -38,6 +38,6 @@ rm jlite_parser.mli jlite_parser.ml jlite_lexer.ml
 ./z.exe assign2_testcases/p_legal_fruitninja.jlite > assign2_testcases_output/p_legal_fruitninja.output
 ./z.exe assign2_testcases/p_legal_overloading.jlite > assign2_testcases_output/p_legal_overloading.output
 ./z.exe assign2_testcases/p_legal_shadowing.jlite > assign2_testcases_output/p_legal_shadowing.output
-./z.exe assign2_testcases/simple_input.jlite > assign2_testcases_output/simple_input.output
-./z.exe assign2_testcases/custom2.jlite > assign2_testcases_output/custom2.output
-./z.exe assign2_testcases/ex1.jlite > assign2_testcases_output/ex1.output
+./z.exe assign2_testcases/sample1.jlite > assign2_testcases_output/sample1.output
+./z.exe assign2_testcases/sample2.jlite > assign2_testcases_output/sample2.output
+./z.exe assign2_testcases/sample3.jlite > assign2_testcases_output/sample3.output
