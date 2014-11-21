dce4a4986690232e5d6a40ec6b6fcf7efdd2d201
acreux <antoine.creux@gmail.com>
2014-11-21 18:04:01 +0800

    Add tests. Add ocaml and makefile

---
+++ code/launch_emu.sh
@@ -0,0 +1,3 @@
+../../sdk/adt-bundle-linux-x86_64-20140702/sdk/tools/emulator -avd Compiler &
+
+
---
+++ code/push.sh
@@ -0,0 +1,3 @@
+
+../../sdk/adt-bundle-linux-x86_64-20140702/sdk/platform-tools/adb push out /data/local/tmp
+../../sdk/adt-bundle-linux-x86_64-20140702/sdk/platform-tools/adb shell
