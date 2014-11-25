97396bba0e324e06a5527ea9f2bbc19e9a6387cb
W. Ellie Vial <wevial@bu.edu>
2014-11-25 20:01:54 +0800

    Add block building for label instruction

--- code/optimize_arm.ml
+++ code/optimize_arm.ml
@@ -3,21 +3,6 @@ open Jlite_structs
 open Ir3_structs
 open Arm_structs
 
-(* Optimization of ARM program *)
-
-let linecount = ref 0
-let fresh_line () = (linecount:=!linecount+1; (string_of_int !linecount))
-
-let blkcount = ref 0
-let fresh_blk () = (blkcount:=!blkcount+1; (string_of_int !blkcount))
-
-let rec last ls =
-  match ls with
-  | [] -> []
-  | l::[] -> l
-  | l::ls -> last ls
-
-
 (* get the basic blocks from the ARM instructions *)
 (* (id, leader flag, instruction ) *)
 type block_instr = int * bool * arm_instr 
@@ -29,81 +14,88 @@ type block =
     id: int;
     label: label;
     mutable blk_instrs: block_instr list;
-    mutable instrs_in: (block_instr * label) list;
-    mutable instrs_out: (block_instr * label) list
+    mutable instrs_in: (block_instr * int * label) list;
+    mutable instrs_out: (block_instr * int * label) list
   }
+
+(* -------- Optimization of ARM program --------- *)
+
+let linecount = ref 0
+let fresh_line () = (linecount:=!linecount+1; !linecount)
+
+let blkcount = ref 0
+(*let fresh_blk () = (blkcount:=!blkcount+1; (string_of_int !blkcount)) *)
+let fresh_blk () = (blkcount:=!blkcount+1; !blkcount)
+
+let rec last ls =
+(*let rec last (ls: (block_instr * int * label) list) = *)
+  match ls with
+  | l::[] -> l
+  | l::ls -> last ls
+  | _ -> failwith " last isn't working right"
+
 
 
 let rec get_basic_blocks instrs (blocks: block list) : block list = []
 
 (* returns updated list of blocks *)
-let rec thisfunction 
+let thisfunction 
   instrs 
   blocks
   current_blk = 
   match instrs with 
+  | [] -> failwith "#1: instruction not implemented in block generation yet"
   | i::is ->
     match i with
     | Label label -> (*create new block*)
-      let block_id = fresh_blk () in
+      let blk_id = fresh_blk () in
       let line_id = fresh_line () in
       (* update current block *)
+      let label_instruction = (line_id, false, i) in
+      let new_blk = {
+        id = blk_id;
+        label = label;
+        blk_instrs = [label_instruction];
+        instrs_in = []; 
+        instrs_out = []
+      } in
       begin
-        let label_instr = (line_id, false, i) in
-        let new_blk = Some {
-          id = block_id;
-          label = label;
-          blk_instrs = [label_instr];
-          instrs_in = []; 
-          instrs_out = []
-        } in
         match current_blk with
-        (* No block exists, create new block *)
-        | None -> [ new_blk ]
-          (*thisfunction [new_block] new_blk *)
-          (* or return the following: *)
-          (* [new_blk] or new_blk *)
-
-        (* Update current block and create new block *)
-        | Some current_blk  ->
-          (* add label instruction to current_blk.instrs_out *)
-          let label_instr = (line_id, false, i) in
-          let last_current_blk_instr = last current_blk.blk_instrs in 
-          current_blk.instrs_out <- List.append current_blk.instrs_out label_instr;
-          (* return list of blocks *)
-          blocks :: [{ id = block_id; label = label; blk_instrs = [label_instr]; instrs_in = []; instrs_out = [] }]
-          (* procede to make that blk - return blocks *)
+        | Some current_blk  ->           (* Update current block and create new block *)
+          let last_instr = last current_blk.blk_instrs in  (* add label instruction to current_blk.instrs_out *)
+           new_blk.instrs_in      <- (last_instr, current_blk.id, current_blk.label) :: []; 
+           current_blk.instrs_out <- List.append current_blk.instrs_out [(label_instruction, blk_id, label)];
+           (blocks :: current_blk :: [], new_blk)     (* return list of blocks and new (current) block *)
+        | None -> ([], new_blk)           (* No block exists, create new block *) (*thisfunction [new_block] new_blk *)
       end
 
-
-
-    | B (cond, label) -> 
-      current_blk.blk_instrs
+(*    | B (cond, label) -> 
+      current_blk.blk_instrs 
         blocks :: [{ id = id; label = label; blk_instrs = []; instrs_in = []; instrs_out = [] }]
-    | BL (cond, label)
+    | BL (cond, label) *)
     | _ -> failwith "#1: instruction not implemented in block generation yet"
-  | _ -> "wow"
+  | _ -> failwith "#3: wow"
 
 
-
+(*
 (* Peephole Optimizations *)
 let rec peephole_opt (instrs: arm_program) : arm_program = 
   let instrs_len = List.length instrs in 
-    match instrs with
+    match instrs_len with
     | 0 -> []
     | 1 -> instrs
     | 2 ->
       let (instr1, instr2) = (List.nth instrs 0, List.nth instrs 1) in
         if (instr1 == instr2) then [instr1]
         else instrs
-    | _ -> []
+    | _ -> [] 
 
 (* Peephole: remove redundant loads and stores *)
 let remove_redundant_load_store instr1 instr2 =
   match instr1, instr2 with
   | LDR load_instr, STR store_instr ->
     begin
-      let (reg_load, addr_type) = x,x  in
+      let (reg_load, addr_type) = load_instr  in
       match load_instr, store_instr with
       | (_,_, reg_ld, addr_type_ld), (_,_, reg_str, addr_type_str) ->
         if (reg_ld == reg_str) && (addr_type_ld == addr_type_str) then
@@ -116,11 +108,11 @@ let remove_redundant_load_store instr1 instr2 =
 
 
 
-
 let optimize_arm (instructions : arm_program) : arm_program =
   let opt1 = peephole_opt instructions 
   in opt1 
 
+*)
 
 
 
