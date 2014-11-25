e9eb7bd54d4ef0aa42464fbf17528b043331d5e5
W. Ellie Vial <wevial@bu.edu>
2014-11-25 20:50:50 +0800

    Add branch instr to block builder

--- code/optimize_arm.ml
+++ code/optimize_arm.ml
@@ -40,9 +40,10 @@ let rec get_basic_blocks instrs (blocks: block list) : block list = []
 
 (* returns updated list of blocks *)
 let thisfunction 
-  instrs 
-  blocks
-  current_blk = 
+  (instrs: arm_program)
+  (blocks: block list)
+  (current_blk: block option)
+  : (block list * block) = 
   match instrs with 
   | [] -> failwith "#1: instruction not implemented in block generation yet"
   | i::is ->
@@ -65,15 +66,47 @@ let thisfunction
           let last_instr = last current_blk.blk_instrs in  (* add label instruction to current_blk.instrs_out *)
            new_blk.instrs_in      <- (last_instr, current_blk.id, current_blk.label) :: []; 
            current_blk.instrs_out <- List.append current_blk.instrs_out [(label_instruction, blk_id, label)];
-           (blocks :: current_blk :: [], new_blk)     (* return list of blocks and new (current) block *)
+
+           (List.append blocks [current_blk], new_blk)     (* return list of blocks and new (current) block *)
+
         | None -> ([], new_blk)           (* No block exists, create new block *) (*thisfunction [new_block] new_blk *)
       end
 
-(*    | B (cond, label) -> 
-      current_blk.blk_instrs 
+    | B (cond, b_label) -> (* add *)
+      let line_id = fresh_line () in
+      let b_instruction = (line_id, false, i) in
+        begin
+          match current_blk with
+          | Some current_blk ->
+
+            current_blk.blk_instrs <- List.append current_blk.blk_instrs [b_instruction];
+            let branch_instr = (b_instruction, current_blk.id, current_blk.label) in 
+              current_blk.instrs_out <- List.append current_blk.instrs_out [branch_instr];
+              if current_blk.label == b_label then 
+                begin
+                  current_blk.instrs_in <- List.append current_blk.instrs_in [branch_instr];
+                  (blocks, current_blk) 
+                end
+              else 
+                let rec helper bs label = (* find block w/ matching label *)
+                  match bs, label with 
+                  | [], _ -> failwith "#5: Block with label wasn't found"
+                  | b::bs, label ->
+                    if b.label == label then
+                      begin
+                        b.instrs_in <- List.append b.instrs_in [branch_instr];
+                        List.append [b] bs 
+                      end
+                    else List.append [b] (helper bs label)
+                in (helper blocks b_label, current_blk) 
+          | None -> failwith "#4: Branch - current block doesn't exist"
+        end
+(*      current.block = []*) 
+
+(*      current_blk.blk_instrs 
         blocks :: [{ id = id; label = label; blk_instrs = []; instrs_in = []; instrs_out = [] }]
     | BL (cond, label) *)
-    | _ -> failwith "#1: instruction not implemented in block generation yet"
+    | _ -> failwith "#1: instruction not implemented in block generation"
   | _ -> failwith "#3: wow"
 
 
